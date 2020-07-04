3d64eec8bb223531b9d957a0c7df1b6234205ecd
alfred schilken <alfred@schilken.de>
2020-07-04 15:24:04 +0200

    add loop over testSet and several k values to example

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,16 @@
 import "package:lodash_chain/lodash_chain.dart";
 import 'data.dart';
 
-void main() {}
+void main() {
+  final testSetSize = 100;
+  final List<List<num>> testSet = alldata.sublist(0, testSetSize);
+  final List<List<num>> trainingSet = alldata.sublist(0, testSetSize);
+
+  for (int k = 1; k < 7; k++) {
+    var successes = testSet.where((List<num> testPoint) =>
+        LodashChain.knn(trainingSet, LodashChain.initial(testPoint), k: k) ==
+        testPoint.last);
+    var accuracy = (successes.length / testSetSize) * 100;
+    print("result for k=$k: $accuracy%");
+  }
+}
